ac8e455636542559d1b25cb2de13b430e749a2a3
kaiqmo <kaiqmo@gmail.com>
2020-01-04 01:00:14 -0300

    Updates

---
+++ precache-manifest.92dcbc8b4732b0a6717f1d2d7e968608.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c053adec5908e53da1f6880f8fb3ac70",
+    "url": "/monster_rolodex/index.html"
+  },
+  {
+    "revision": "6d04017cf08702d46847",
+    "url": "/monster_rolodex/static/css/main.38110631.chunk.css"
+  },
+  {
+    "revision": "25f4a5800515bf04f7ae",
+    "url": "/monster_rolodex/static/js/2.6e883175.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster_rolodex/static/js/2.6e883175.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d04017cf08702d46847",
+    "url": "/monster_rolodex/static/js/main.ebaa3ee2.chunk.js"
+  },
+  {
+    "revision": "c73c9c328c59e6a7b54d",
+    "url": "/monster_rolodex/static/js/runtime-main.bf14c6de.js"
+  }
+]);
